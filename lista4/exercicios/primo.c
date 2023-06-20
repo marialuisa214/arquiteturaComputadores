031878320b7285b3e880f3df6c0cbe56539bc9cf
Maria Luisa <211039617@aluno.unb.br>
2023-06-20 10:25:35 -0300

    :running: atividade 1 em código .c

--- lista4/exercicios/primo.c
+++ lista4/exercicios/primo.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
 
-int main(){}
+int main() {
+    int numero;
+    scanf("%d", &numero);
+
+    if (numero <= 0) {
+        printf("Entrada inválida\n");
+        return 0;
+    }
+
+    if (numero == 1) {
+        printf("Não é primo\n");
+        return 0;
+    }
+
+    for (int i = 2; i * i <= numero; i++) {
+        int resto = numero % i;
+        if (resto == 0) {
+            printf("Não é primo\n");
+            return 0;
+        }
+    }
+    printf("É primo\n");
+}
